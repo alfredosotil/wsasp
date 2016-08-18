045c1cb18ad2f5864a5281122d1df34996da8031
asotilp <asotilp@asotilp-01.olo.com.local>
2016-08-18 10:27:19 -0500

    commit version 1.0 trabajo
    - dev commit
    - web site alfredo sotil pastor

---
+++ javascript/custom.js
@@ -0,0 +1,12 @@
+$('a#readMore').on('click', function (e) {
+    e.preventDefault();
+    var $this = $(this);
+    var $collapse = $this.closest('.collapse-group').find('.collapse');
+    $collapse.collapse('toggle');
+    if($this.text() == 'Show More'){
+        $this.text('Show Less');
+    }else{
+        $this.text('Show More');
+    }
+});
+
